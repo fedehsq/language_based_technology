07efd47931b385893355277db07850d873e55ae8
fedehsq <fedebernacca@gmail.com>
2021-04-21 10:12:27 +0200

    jsflow ended

--- jsonflow.js
+++ jsonflow.js
@@ -53,7 +53,7 @@ h1 = h6;
 l6 = declassify(h1);
 
 
-
+// 4
 try {
     l1 = false;
     if (h1) {
@@ -117,7 +117,7 @@ try {
     skip;
   }
 
-
+// 5
   let (x = h1) in l1 = x;
 let (x = h2) in l2 = x;
 let (x = h3) in l3 = x;
@@ -125,7 +125,7 @@ let (x = h4) in l4 = x;
 let (x = h5) in l5 = x;
 let (x = h6) in l6 = x;
 
-
+// 6
 declare proc p(in x : low, out y : low) { y = x; }
 if (h1) p(true, l1); else p(false, l1);
 if (h2) p(true, l2); else p(false, l2);
@@ -133,5 +133,74 @@ if (h3) p(true, l3); else p(false, l3);
 if (h4) p(true, l4); else p(false, l4);
 if (h5) p(true, l5); else p(false, l5);
 if (h6) p(true, l6); else p(false, l6);
-  
-      
+
+// 7
+declare ref hp : low; 
+
+ref hp = h1;
+l1 = deref(hp);
+
+ref hp = h2;
+l2 = deref(hp);
+
+ref hp = h3;
+l3 = deref(hp);
+
+ref hp = h4;
+l4 = deref(hp);
+
+ref hp = h5;
+l5 = deref(hp);
+
+ref hp = h6;
+l6 = deref(hp);
+
+
+// 8
+declare array larray : low;
+
+larray[h1] = true;
+larray[!h1] = false;
+t = larray[true];
+if (t) l1 = true; else l1 = false;
+
+larray[h2] = true;
+larray[!h2] = false;
+t = larray[true];
+if (t) l2 = true; else l2 = false;
+
+larray[h3] = true;
+larray[!h3] = false;
+t = larray[true];
+if (t) l3 = true; else l3 = false;
+
+larray[h4] = true;
+larray[!h4] = false;
+t = larray[true];
+if (t) l4 = true; else l4 = false;
+
+larray[h5] = true;
+larray[!h5] = false;
+t = larray[true];
+if (t) l5 = true; else l5 = false;
+
+larray[h6] = true;
+larray[!h6] = false;
+t = larray[true];
+if (t) l6 = true; else l6 = false;
+
+// 9
+l1 = true;
+l2 = false;
+l3 = true;
+l4 = false;
+l5 = true;
+l6 = true;
+
+// 10
+l1 = true;
+l2 = false;
+l3 = true;
+l4 = false;
+l5 = false;
+l6 = true;
